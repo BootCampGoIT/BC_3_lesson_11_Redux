28445436d2b6dbb01e339f45a7d0190ba817e334
AndriiShnurkov <Shans12.84@ukr.net>
2021-01-29 09:18:11 +0200

    added template

---
+++ src/components/bootCamp/bootCampForm/BootCampForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const BootCampForm = () => {
+    return (
+        <form>
+            <input type="text"/>
+
+        </form>
+    );
+}
+
+export default BootCampForm;
---
+++ src/components/bootCamp/bootCampList/BootCampList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BootCampList = () => {
+    return (
+        <ul>
+        
+
+        </ul>
+    );
+}
+
+export default BootCampList;
